668c3ef5d3f8da9734553a41378125482886a930
Sergei Shulepov <sergei@parity.io>
2021-03-05 20:12:37 +0000

    WIP

--- node/core/pvf/src/execute/worker.rs
+++ node/core/pvf/src/execute/worker.rs
@@ -1,4 +1,18 @@
-use std::time::Instant;
+// Copyright 2021 Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::{
 	artifacts::Artifact,
@@ -7,7 +21,7 @@ use crate::{
 		spawn_with_program_path,
 	},
 };
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use async_std::{
 	io,
 	os::unix::net::UnixStream,
